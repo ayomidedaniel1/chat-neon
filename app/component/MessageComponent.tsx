c6534a974770b9a418c56a6e24040686b9b77de5
Ayomide <ayomidedans@gmail.com>
2023-12-10 14:20:15 +0100

    fix: navigation issues

--- app/component/MessageComponent.tsx
+++ app/component/MessageComponent.tsx
@@ -1,7 +1,7 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { Ionicons } from "@expo/vector-icons";
-import { styles } from "../utils/styles";
+import { styles } from "../utils/style";
 
 export default function MessageComponent({ item, user }) {
   const status = item.user !== user;
